f626bf45837b103bc5914f3fd63aed1b920cc0cb
Philip Chukwuma <cchukwuma.p@gmail.com>
2023-01-31 19:46:26 +0000

    cleanup

--- PhilsPOSWeb/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PhilsPOSWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
