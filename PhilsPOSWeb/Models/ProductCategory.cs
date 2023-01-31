2cd060ea6d30f996e77b606f781a6e92fb56dd90
Philip Chukwuma <cchukwuma.p@gmail.com>
2023-01-31 19:08:02 +0000

    Project setup

---
+++ PhilsPOSWeb/Models/ProductCategory.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PhilsPOSWeb.Models
+{
+    public class ProductCategory
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
