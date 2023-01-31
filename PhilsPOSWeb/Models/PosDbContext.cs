6cef1547b48116a17da4c36744e0b8d8ef2737a7
Philip Chukwuma <cchukwuma.p@gmail.com>
2023-01-31 19:48:21 +0000

    commit staged

---
+++ PhilsPOSWeb/Models/PosDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace PhilsPOSWeb.Models
+{
+    public class PosDbContext : IdentityDbContext
+    {
+        public PosDbContext(DbContextOptions<PosDbContext>options) : base(options)
+        {
+
+        }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
